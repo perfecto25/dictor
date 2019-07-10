c6a3c1b996c245c6f24ea5f34684064c03e3ff42
mreider <mike.reider@gmail.com>
2019-07-10 17:06:02 -0400

    eval fix

--- tests/test.1.py
+++ tests/test.1.py
@@ -1,13 +1,27 @@
 
 #!/usr/bin/env python
 # coding=utf-8
+'''
+DICTOR testing suite
+shell> nosetest test.py
+'''
 
 from __future__ import print_function
+import json
 import sys
+sys.path.append("..")
 
 # import Reduce if py3
 if sys.version_info[0] == 3:
     from functools import reduce
+
+with open('basic.json') as data:
+    BASIC = json.load(data)
+
+with open('list.json') as data:
+    LIST = json.load(data)
+
+
 
 def dictor(data, path=None, default=None, checknone=False):
     '''
@@ -39,15 +53,55 @@ def dictor(data, path=None, default=None, checknone=False):
 
     value = None
     keys = path.split(".")
+
+    # # reset path
+    # path = None
+
+    # # if 1st key is a list index
+    # if all(char.isdigit() for char in keys[0]):
+    #     path = '['+keys[0]+']'
+
+    #     # remove 1st key from key list
+    #     keys.pop(0)
+    print(keys)
+    print(default)
+    # build proper path
+    for key in keys:
+        # check if key is a list
+        if key.endswith(']'):
+            temp = key.split('[')
+            key = ''.join(temp[0])
+            index = int(temp[1].strip(']'))
+            if path is None:
+                path = "['"+key+"']"+"["+str(index)+"]"
+            else:
+                path = path+"['"+key+"']"+"["+str(index)+"]"
+        else:
+            if path is None:
+                path = "['"+key+"']"
+
+            else:
+                # check if key is an index
+                if key.isdigit() is True:
+                    path = path + "["+key+"]"
+                else:
+                    path = path + "['"+key+"']"
   
-    try:        
+    try:
+
         value = reduce(lambda c, k: c.get(k, {}), keys, data)
-        if value is None:
-            value = default
+        print('----')
+        print(value)
     except (KeyError, ValueError, IndexError, TypeError) as err:
         value = default
     finally:
         if checknone:
             if not value:
                 raise ValueError('missing value for %s' % path)
+        if bool(value) is False or value is None:
+            value = default
         return value
+
+
+
+print(dictor(LIST, '0.name'))
