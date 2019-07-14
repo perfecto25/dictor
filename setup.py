0ba5d354bb834c6f9bd43abe2576ed8056058ee1
perfecto25 <mike.reider@gmail.com>
2019-07-13 22:13:40 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
 setup(name='dictor',
       version='0.1.1',
       description='an elegant dictionary and JSON handler',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/perfecto25/dictor',
       author='mike.reider',
       author_email='mike.reider@gmail.com',
