57bb4379df33e6b8f4590e2e0678a4397fed2af9
mreider <mike.reider@gmail.com>
2019-10-21 15:09:13 -0400

    0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
       long_description = fh.read()
 
 setup(name='dictor',
-      version='0.1.2',
+      version='0.1.3',
       description='an elegant dictionary and JSON handler',
       long_description=long_description,
       long_description_content_type="text/markdown",
