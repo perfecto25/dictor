4d93c4a901b9072f5ca0a7108b756dd32699e9c2
perfecto25 <mike.reider@gmail.com>
2020-02-04 23:30:53 -0500

    0.1.5

--- dictor/__init__.py
+++ dictor/__init__.py
@@ -9,27 +9,20 @@ def dictor(data, path=None, default=None, checknone=False, ignorecase=False, pat
     Usage:
     get a value from a Dictionary key
     > dictor(data, "employees.John Doe.first_name")
-
     parse key index and fallback on default value if None,
     > dictor(data, "employees.5.first_name", "No employee found")
-
     pass a parameter
     > dictor(data, "company.{}.address".format(my_company))
-
     if using Python 3, can use F-strings to pass parameter
     > param = 'MyCompany'
     > dictor(data, f"company.{param}.address")
-
     lookup a 3rd element of List, on second key, lookup index=5
     > dictor(data, "3.first.second.5")
-
     lookup a nested list of lists
     > dictor(data, "0.first.1.2.second.third.0.2"
-
     check if return value is None, if it is, raise an error
     > dictor(data, "some.key.value", checknone=True)
     > ValueError: value not found for search path: "some.key.value"
-
     ignore letter casing when searching
     > dictor(data, "employees.Fred Flintstone", ignorecase=True)
     '''
@@ -55,5 +48,5 @@ def dictor(data, path=None, default=None, checknone=False, ignorecase=False, pat
             
     if checknone:
         if val is None or val == default:
-            raise ValueError('value not found for search path: "%s"' % path)
+            raise ValueError('value not found for search path: "%s"' % path)        
     return val
